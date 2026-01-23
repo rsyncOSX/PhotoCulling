a31988b58a5adcc3d8c4ecf4bc2b6de46ca198d3
Thomas Evensen <thomeven@gmail.com>
2026-01-23 19:43:41 +0100

    Update extensionSidebarPhotoCullingView.swift

--- PhotoCulling/Main/extensionSidebarPhotoCullingView.swift
+++ PhotoCulling/Main/extensionSidebarPhotoCullingView.swift
@@ -18,9 +18,6 @@ extension SidebarPhotoCullingView {
                     cullingmanager.selectedFiles.removeAll()
                     cullingmanager.numberofPreselectedFiles.removeAll()
                     cullingmanager.saveToJSON()
-                    Task {
-                        await ThumbnailProvider.shared.clearCaches()
-                    }
                 }
                 label: { Image(systemName: "trash.fill") }
                 .help("Clear preseselcted files")
