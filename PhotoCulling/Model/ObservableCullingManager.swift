17034731cddee48d1b2e65accd2f66b7a94871c4
Thomas Evensen <thomeven@gmail.com>
2026-01-28 08:37:27 +0100

    Update ObservableCullingManager.swift

--- PhotoCulling/Model/ObservableCullingManager.swift
+++ PhotoCulling/Model/ObservableCullingManager.swift
@@ -20,42 +20,65 @@ final class ObservableCullingManager {
         }
     }
 
+    private func verifytoggleSelectionSavedFiles(in arwcatalog: URL, toggledfilename: String) -> Bool {
+        if let index = savedFiles.firstIndex(where: { $0.catalog == arwcatalog }) {
+            if savedFiles[index].filerecords == nil { return false
+            } else {
+                let verify = savedFiles[index].filerecords?.filter { $0.fileName == toggledfilename }
+                if verify?.isEmpty == false {
+                    // Existing photo, remove photo
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
     func toggleSelectionSavedFiles(in fileurl: URL?, toggledfilename: String) {
         if let fileurl {
-            let newrecord = FileRecord(
-                fileName: toggledfilename,
-                dateTagged: Date().en_string_from_date(),
-                dateCopied: nil
-            )
-
             let arwcatalog = fileurl.deletingLastPathComponent()
 
-            if savedFiles.isEmpty {
-                let savedfiles = SavedFiles(
-                    catalog: arwcatalog,
-                    dateStart: Date().en_string_from_date(),
-                    filerecord: newrecord
-                )
-                savedFiles.append(savedfiles)
-            } else {
-                // Check if arw catalog exists in data structure
+            if verifytoggleSelectionSavedFiles(in: arwcatalog, toggledfilename: toggledfilename) {
+                // Remove item
                 if let index = savedFiles.firstIndex(where: { $0.catalog == arwcatalog }) {
-                    if savedFiles[index].filerecords == nil {
-                        savedFiles[index].filerecords = [newrecord]
-                    } else {
-                        savedFiles[index].filerecords?.append(newrecord)
+                    savedFiles[index].filerecords?.removeAll { record in
+                        record.fileName == toggledfilename
                     }
-                } else {
-                    // If not append a new one
+                }
+            } else {
+                // New item
+                let newrecord = FileRecord(
+                    fileName: toggledfilename,
+                    dateTagged: Date().en_string_from_date(),
+                    dateCopied: nil
+                )
+
+                if savedFiles.isEmpty {
                     let savedfiles = SavedFiles(
                         catalog: arwcatalog,
                         dateStart: Date().en_string_from_date(),
                         filerecord: newrecord
                     )
                     savedFiles.append(savedfiles)
+                } else {
+                    // Check if arw catalog exists in data structure
+                    if let index = savedFiles.firstIndex(where: { $0.catalog == arwcatalog }) {
+                        if savedFiles[index].filerecords == nil {
+                            savedFiles[index].filerecords = [newrecord]
+                        } else {
+                            savedFiles[index].filerecords?.append(newrecord)
+                        }
+                    } else {
+                        // If not append a new one
+                        let savedfiles = SavedFiles(
+                            catalog: arwcatalog,
+                            dateStart: Date().en_string_from_date(),
+                            filerecord: newrecord
+                        )
+                        savedFiles.append(savedfiles)
+                    }
                 }
             }
-
             WriteSavedFilesJSON(savedFiles)
         }
     }
